af1fc62dd58144b60487df690c77dcf8daa45669
cterech <cterech@gmail.com>
2022-09-13 14:46:05 -0400

    add Goerli schema

--- dist/schemas/index.js
+++ dist/schemas/index.js
@@ -4,7 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.schemas = void 0;
 const index_1 = require("./main/index");
 const index_2 = require("./rinkeby/index");
+const index_3 = require("./goerli/index");
 exports.schemas = {
+    goerli: index_3.goerliSchemas,
     rinkeby: index_2.rinkebySchemas,
     main: index_1.mainSchemas,
 };
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,8 +2,10 @@
 
 import { mainSchemas } from './main/index';
 import { rinkebySchemas } from './rinkeby/index';
+import { goerliSchemas } from './goerli/index';
 
 export const schemas = {
+  goerli: goerliSchemas,
   rinkeby: rinkebySchemas,
   main: mainSchemas,
 };
